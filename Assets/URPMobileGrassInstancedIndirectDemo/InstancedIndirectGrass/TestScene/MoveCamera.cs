a542d8463fd4da0291e7de405abd4c9f11c0f037
ColinLeung-NiloCat <7102676+ColinLeung-NiloCat@users.noreply.github.com>
2020-07-26 15:01:00 +0800

    add more camera pos, rebake lighting

---
+++ Assets/URPMobileGrassInstancedIndirectDemo/InstancedIndirectGrass/TestScene/MoveCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Camera camera;
+    public List<Transform> allCameraTransforms = new List<Transform>();
+
+    int index = 0;
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(100, 750, 200, 100), "Move camera"))
+        {
+            index = (index+1) % allCameraTransforms.Count;
+            Transform current = allCameraTransforms[index];
+            camera.transform.position = current.position;
+            camera.transform.rotation = current.rotation;
+        }
+    }
+}
